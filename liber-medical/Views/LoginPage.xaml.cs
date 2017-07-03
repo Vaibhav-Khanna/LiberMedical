7f40a96e69836467bf2b2755603f3499996c0e63
doxpi <doxpibtz@gmail.com>
2017-07-03 17:08:14 +0200

    Project initialization , LoginPage.xaml

---
+++ liber-medical/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using libermedical.CustomControls;
+using Xamarin.Forms;
+
+namespace libermedical.Views
+{
+    public partial class LoginPage : BasePage    
+    {
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
