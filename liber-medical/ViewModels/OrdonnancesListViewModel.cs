0293da6e50c620caed951c28be632f797e506746
Dhillu <dhillu143@gmail.com>
2017-08-17 22:21:00 +0530

    moving code from code behind to viewmodel of OrdonnancesList

--- liber-medical/ViewModels/OrdonnancesListViewModel.cs
+++ liber-medical/ViewModels/OrdonnancesListViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System.Threading.Tasks;
 using libermedical.Models;
+using libermedical.Pages;
 using libermedical.Services;
+using libermedical.Utility;
 using libermedical.ViewModels.Base;
+using Plugin.Media;
 using Xamarin.Forms;
 
 namespace libermedical.ViewModels
@@ -17,5 +20,48 @@ namespace libermedical.ViewModels
 			var ctx = cell.BindingContext;
 			await CoreMethods.PushPageModelWithNewNavigation<DetailsPatientListViewModel>(ctx);
 		}
+
+		public System.Windows.Input.ICommand OpenCameraCommand => new Command(async () =>
+		{
+			var action = await CoreMethods.DisplayActionSheet(null, "Annuler", null, "Ordonnance rapide", "Ordonnance classique");
+			var typeDoc = "ordonnance";
+			string typeNavigation = "";
+
+			if ((action != null) && (action != "Annuler"))
+			{
+				//init du plugin photo 
+				await CrossMedia.Current.Initialize();
+
+				if (UtilityClass.CameraAvailable())
+				{
+
+					var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions()
+					{
+						AllowCropping = true
+					});
+
+					if (file != null)
+					{
+
+						var profilePicture = ImageSource.FromStream(() => file.GetStream());
+
+						if (action == "Ordonnance rapide") { typeNavigation = "fast"; }
+						if (action == "Ordonnance classique") { typeNavigation = "normal"; }
+
+                        //await CoreMethods.PushPageModel<PatientListPage>(new NavigationPage(new PatientListPage(typeNavigation, typeDoc)),true);
+                       // await CoreMethods.PushPageModelWithNewNavigation<PatientListPage>(null, true);
+
+					}
+				}
+				else
+				{
+					await CoreMethods.DisplayAlert("No Camera", ":( No camera available.", "OK");
+				}
+			}
+			else
+			{
+				return;
+			}
+		});
 	}
 }
