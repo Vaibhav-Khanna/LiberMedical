279658c2534c7d5b60870963d6a2d5a4628f3e3e
Amal-Sher Kurbanov <amalsher.kurbanov@gmail.com>
2017-08-27 19:45:40 +0600

    #25 Added splash screen on android

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,11 +1,10 @@
-﻿
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
 namespace libermedical.Droid
 {
-	[Activity(Label = "liber-medical.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "Liber Medical", Icon = "@drawable/icon", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
