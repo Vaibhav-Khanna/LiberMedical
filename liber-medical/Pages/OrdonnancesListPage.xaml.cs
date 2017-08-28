dd38b04732f7871f2088348595754c5d3a5d4d4d
Amal-Sher Kurbanov <amalsher.kurbanov@gmail.com>
2017-08-28 22:10:28 +0600

    #35 and #36 show the prescriptions list and implement the search

--- liber-medical/Pages/OrdonnancesListPage.xaml.cs
+++ liber-medical/Pages/OrdonnancesListPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using libermedical.CustomControls;
 using libermedical.Models;
 using libermedical.Utility;
@@ -8,86 +9,90 @@ using Xamarin.Forms;
 
 namespace libermedical.Pages
 {
-	public partial class OrdonnancesListPage : BasePage
+    public partial class OrdonnancesListPage : BasePage
     {
-        public ObservableCollection<Ordonnance> ordonnances { get; set; }
+        public ObservableCollection<Ordonnance> Ordonnances { get; set; }
 
         public OrdonnancesListPage() : base(0, 0)
         {
-
             BindingContext = this;
-            ordonnances = new ObservableCollection<Ordonnance>
+            Ordonnances = new ObservableCollection<Ordonnance>
             {
-                new Ordonnance {
-                    Reference= 1,
-                    Patient= new Patient {FirstName= "Fred",LastName="Pearson"},
-                    AddDate= new DateTime(2017, 04, 02),
+                new Ordonnance
+                {
+                    Reference = 1,
+                    Patient = new Patient {FirstName = "Fred", LastName = "Pearson"},
+                    AddDate = new DateTime(2017, 04, 02),
                     Status = "Traité"
                 },
-                new Ordonnance {
-                    Reference= 2,
-                    Patient= new Patient {FirstName= "Jonanthan",LastName="Vaughn"},
-                    AddDate= new DateTime(2017, 11, 10),
+                new Ordonnance
+                {
+                    Reference = 2,
+                    Patient = new Patient {FirstName = "Jonanthan", LastName = "Vaughn"},
+                    AddDate = new DateTime(2017, 11, 10),
                     Status = "Traité"
                 },
-                new Ordonnance {
-                    Reference= 3,
-                    Patient= new Patient {FirstName= "Alexander",LastName="Zimmerman"},
-                    AddDate= new DateTime(2017, 02, 06),
+                new Ordonnance
+                {
+                    Reference = 3,
+                    Patient = new Patient {FirstName = "Alexander", LastName = "Zimmerman"},
+                    AddDate = new DateTime(2017, 02, 06),
                     Status = "Refusé"
                 },
-                new Ordonnance {
-                    Reference= 4,
-                    Patient= new Patient {FirstName= "Keith",LastName="Kelley"},
-                    AddDate= new DateTime(2017, 09, 17),
+                new Ordonnance
+                {
+                    Reference = 4,
+                    Patient = new Patient {FirstName = "Keith", LastName = "Kelley"},
+                    AddDate = new DateTime(2017, 09, 17),
                     Status = "Refusé"
                 },
-                new Ordonnance {
-                    Reference= 5,
-                    Patient= new Patient {FirstName= "Justin",LastName="Sims"},
-                    AddDate= new DateTime(2017, 03, 04),
+                new Ordonnance
+                {
+                    Reference = 5,
+                    Patient = new Patient {FirstName = "Justin", LastName = "Sims"},
+                    AddDate = new DateTime(2017, 03, 04),
                     Status = "Traité"
                 },
-                new Ordonnance {
-                    Reference= 6,
-                    Patient= new Patient {FirstName= "Franklin",LastName="Howard"},
-                    AddDate= new DateTime(2017, 09, 29),
+                new Ordonnance
+                {
+                    Reference = 6,
+                    Patient = new Patient {FirstName = "Franklin", LastName = "Howard"},
+                    AddDate = new DateTime(2017, 09, 29),
                     Status = "En attente"
                 },
-                new Ordonnance {
-                    Reference= 7,
-                    Patient= new Patient {FirstName= "Franklin",LastName="Howard"},
-                    AddDate= new DateTime(2017, 09, 29),
-                    Status = "Refusé"
-                },
-                new Ordonnance {
-                    Reference= 8,
-                    Patient= new Patient {FirstName= "Mickael",LastName="Green"},
-                    AddDate= new DateTime(2017, 09, 29),
+                new Ordonnance
+                {
+                    Reference = 8,
+                    Patient = new Patient {FirstName = "Mickael", LastName = "Green"},
+                    AddDate = new DateTime(2017, 09, 29),
                     Status = "En attente"
                 },
-                new Ordonnance {
-                    Reference= 9,
-                    Patient= new Patient {FirstName= "Paul",LastName="Howard"},
-                    AddDate= new DateTime(2017, 09, 29),
+                new Ordonnance
+                {
+                    Reference = 9,
+                    Patient = new Patient {FirstName = "Paul", LastName = "Howard"},
+                    AddDate = new DateTime(2017, 09, 29),
                     Status = "Traité"
                 },
-                new Ordonnance {
-                    Reference= 10,
-                    Patient= new Patient {FirstName= "Justin",LastName="Howard"},
-                    AddDate= new DateTime(2017, 10, 29),
+                new Ordonnance
+                {
+                    Reference = 10,
+                    Patient = new Patient {FirstName = "Justin", LastName = "Howard"},
+                    AddDate = new DateTime(2017, 10, 29),
                     Status = "En attente"
                 },
-                new Ordonnance {
-                    Reference= 11,
-                    Patient= new Patient {FirstName= "John",LastName="Obama"},
-                    AddDate= new DateTime(2017, 09, 29),
+                new Ordonnance
+                {
+                    Reference = 11,
+                    Patient = new Patient {FirstName = "John", LastName = "Obama"},
+                    AddDate = new DateTime(2017, 09, 29),
                     Status = "En attente"
                 }
             };
 
-
             InitializeComponent();
+
+            MyListView.ItemsSource = Ordonnances;
         }
 
 
@@ -100,14 +105,14 @@ namespace libermedical.Pages
         {
             await Navigation.PushModalAsync(new FilterPage());
         }
+
         void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
         {
-            ((ListView)sender).SelectedItem = null;
+            ((ListView) sender).SelectedItem = null;
         }
 
         async void Add_Clicked(object sender, System.EventArgs e)
         {
-
             var action = await DisplayActionSheet(null, "Annuler", null, "Ordonnance rapide", "Ordonnance classique");
             var typeDoc = "ordonnance";
             string typeNavigation = "";
@@ -119,22 +124,27 @@ namespace libermedical.Pages
 
                 if (UtilityClass.CameraAvailable())
                 {
-
-                    var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions()
-                    {
-                        AllowCropping = true
-                    });
+                    var file = await CrossMedia.Current.TakePhotoAsync(
+                        new Plugin.Media.Abstractions.StoreCameraMediaOptions()
+                        {
+                            AllowCropping = true
+                        });
 
                     if (file != null)
                     {
-
                         var profilePicture = ImageSource.FromStream(() => file.GetStream());
 
-                        if (action == "Ordonnance rapide") { typeNavigation = "fast"; }
-                        if (action == "Ordonnance classique") { typeNavigation = "normal"; }
+                        if (action == "Ordonnance rapide")
+                        {
+                            typeNavigation = "fast";
+                        }
+                        if (action == "Ordonnance classique")
+                        {
+                            typeNavigation = "normal";
+                        }
 
-                        await Navigation.PushModalAsync(new NavigationPage(new PatientListPage(typeNavigation, typeDoc)));
-
+                        await Navigation.PushModalAsync(
+                            new NavigationPage(new PatientListPage(typeNavigation, typeDoc)));
                     }
                 }
                 else
@@ -145,6 +155,20 @@ namespace libermedical.Pages
             else
             {
                 return;
+            }
+        }
+
+        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+            {
+                var foundItems =
+                    Ordonnances.Where(x => x.Patient.FullName.ToLower().Contains(e.NewTextValue.ToLower()));
+                MyListView.ItemsSource = foundItems;
+            }
+            else
+            {
+                MyListView.ItemsSource = Ordonnances;
             }
         }
     }
