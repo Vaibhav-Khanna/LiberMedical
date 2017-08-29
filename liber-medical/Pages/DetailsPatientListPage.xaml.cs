fa74a4d83b2cf7362aed04495df77cbf5b4228a6
Dhillu <dhillu143@gmail.com>
2017-08-29 15:28:58 +0530

    revamped

--- liber-medical/Pages/DetailsPatientListPage.xaml.cs
+++ liber-medical/Pages/DetailsPatientListPage.xaml.cs
@@ -8,14 +8,23 @@ namespace libermedical.Pages
 {
 	public partial class DetailsPatientListPage
     {
-
-
         public DetailsPatientListPage() : base(-1, 64, false)
         {
             InitializeComponent();
+
         }
 
-       
+        async void Back_Tapped(object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void Edit_Tapped(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new PatientDetailModify());
+        }
+
+
         void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
         {
             ((ListView)sender).SelectedItem = null;
