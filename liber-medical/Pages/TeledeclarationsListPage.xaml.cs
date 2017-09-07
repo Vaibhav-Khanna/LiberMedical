ce4e820a6b001e92de859a2779390a9c6745f43a
Pavel Mesenev <mesenev.pr@gmail.com>
2017-09-07 11:45:29 +1000

    remove spacing between navbar and content on tabbed pages

--- liber-medical/Pages/TeledeclarationsListPage.xaml.cs
+++ liber-medical/Pages/TeledeclarationsListPage.xaml.cs
@@ -1,108 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using libermedical.CustomControls;
 using libermedical.Models;
 using Xamarin.Forms;
 
 namespace libermedical.Pages
 {
-    public partial class TeledeclarationsListPage : BasePage
-    {
-        public ObservableCollection<Teledeclaration> teledeclarations { get; set; }
-        public TeledeclarationsListPage() : base(0, 64)
-        {
-            BindingContext = this;
-            teledeclarations = new ObservableCollection<Teledeclaration>
-            {
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 04, 02),
-                    TotalAccount= 92.76,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 11, 08),
-                    TotalAccount= 67.64,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 04, 02),
-                    TotalAccount= 92.76,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 04, 02),
-                    TotalAccount= 92.76,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 04, 02),
-                    TotalAccount= 92.76,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 05, 24),
-                    TotalAccount= 92.76,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2016, 07, 02),
-                    TotalAccount= 92.76,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 02, 11),
-                    TotalAccount= 92.76,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 03, 30),
-                    TotalAccount= 92.76,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 10, 22),
-                    TotalAccount= 12.64,
-                    Status = "Traité"
-                },
-                new Teledeclaration {
-                    Reference= 1,
-                    AddDate= new DateTime(2017, 08, 02),
-                    TotalAccount= 145.32,
-                    Status = "Traité"
-                }
-            };
+	public partial class TeledeclarationsListPage
+	{
+		public TeledeclarationsListPage()
+		{
+			BindingContext = this;
+			teledeclarations = new ObservableCollection<Teledeclaration>
+			{
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 04, 02),
+					TotalAccount = 92.76,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 11, 08),
+					TotalAccount = 67.64,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 04, 02),
+					TotalAccount = 92.76,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 04, 02),
+					TotalAccount = 92.76,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 04, 02),
+					TotalAccount = 92.76,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 05, 24),
+					TotalAccount = 92.76,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2016, 07, 02),
+					TotalAccount = 92.76,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 02, 11),
+					TotalAccount = 92.76,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 03, 30),
+					TotalAccount = 92.76,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 10, 22),
+					TotalAccount = 12.64,
+					Status = "Traité"
+				},
+				new Teledeclaration
+				{
+					Reference = 1,
+					AddDate = new DateTime(2017, 08, 02),
+					TotalAccount = 145.32,
+					Status = "Traité"
+				}
+			};
 
-            InitializeComponent();
-        }
+			InitializeComponent();
+		}
 
-        async void Filter_Clicked(object sender, System.EventArgs e)
-        {
-            await Navigation.PushModalAsync(new FilterPage());
-        }
+		public ObservableCollection<Teledeclaration> teledeclarations { get; set; }
 
-        async void Teledeclaration_Tapped(object sender, System.EventArgs e)
-        {
-            await Navigation.PushModalAsync(new NavigationPage(new TeledeclarationSecureActionPage()));
-        }
+		private async void Filter_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PushModalAsync(new FilterPage());
+		}
 
-        async void Bill_Tapped(object sender, System.EventArgs e)
-        {
-            await Navigation.PushModalAsync(new SecuriseBillsPage());
-        }
-        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
-        {
-            ((ListView)sender).SelectedItem = null;
-        }
-    }
+		private async void Teledeclaration_Tapped(object sender, EventArgs e)
+		{
+			await Navigation.PushModalAsync(new NavigationPage(new TeledeclarationSecureActionPage()));
+		}
+
+		private async void Bill_Tapped(object sender, EventArgs e)
+		{
+			await Navigation.PushModalAsync(new SecuriseBillsPage());
+		}
+
+		private void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			((ListView) sender).SelectedItem = null;
+		}
+	}
 }
