eb58c8171d8a01c310ef124cf2e920e9f920923e
Dhillu <dhillu143@gmail.com>
2017-09-20 21:44:41 +0530

    Contact my advisor implemented

--- liber-medical/ViewModels/PlusViewModel.cs
+++ liber-medical/ViewModels/PlusViewModel.cs
@@ -9,9 +9,18 @@ namespace libermedical.ViewModels
 		public ICommand GoToProfileCommand => new Command(async () => 
 			await CoreMethods.PushPageModel<MyAccountViewModel>(null, true));
 
-		public ICommand ConnectCommand => new Command(() =>
+		public ICommand ConnectCommand => new Command(async () =>
 		{
-			var action = CoreMethods.DisplayActionSheet("Contacter mon conseiller via:", "Annuler", null, "Appel vocal", "SMS");
-		});
+			var action = await CoreMethods.DisplayActionSheet("Contacter mon conseiller via:", "Annuler", null, "Appel vocal", "SMS");
+            switch (action)
+            {
+                case "Appel vocal":
+                    Device.OpenUri(new System.Uri("tel:+33559311824"));
+                    break;
+                case "SMS":
+                    Device.OpenUri(new System.Uri("sms:+33559311824"));
+                    break;
+            }
+        });
 	}
 }
