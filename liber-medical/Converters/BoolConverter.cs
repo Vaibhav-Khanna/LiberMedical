fb614fe6d167d944e3c45c55c4dc83204b7e608f
Dhillu <dhillu143@gmail.com>
2017-09-26 11:02:58 +0530

    Added JsonConverter and Updated UI in Ordonnances & Teledeclarations

--- liber-medical/Converters/BoolConverter.cs
+++ liber-medical/Converters/BoolConverter.cs
@@ -20,4 +20,22 @@ namespace libermedical.Converters
 			return objectType == typeof(bool);
 		}
 	}
+
+	public class BoolSyncConverter : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			writer.WriteValue(value);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			return reader.Value ?? true;
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return objectType == typeof(bool);
+		}
+	}
 }
---
+++ liber-medical/Converters/StatusColorConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace libermedical.Converters
+{
+	public class StatusColorConverter : IValueConverter, IMarkupExtension
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var status = value.ToString();
+
+			if (status == "En attente")
+				return "#f6a623";
+			else if (status == "Traité")
+				return "#d1d1d1";
+			else if (status == "Refusé")
+				return "#ff7586";
+			else
+				return "#ff7586";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return this;
+		}
+	}
+}
