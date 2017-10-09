b54ff923f7948bbe024f727e3bf76ab27430b6e1
dhillu <dhillu@DHILLUs-MacBook-Pro.local>
2017-10-09 21:20:59 +0530

    fixed quick prescription and document

--- liber-medical/ViewModels/HomeViewModel.cs
+++ liber-medical/ViewModels/HomeViewModel.cs
@@ -14,176 +14,177 @@ using libermedical.Request;
 
 namespace libermedical.ViewModels
 {
-    public class HomeViewModel : ViewModelBase
-    {
-        //type of photo, most of the time ordonannces but we can also have document
-        //so we initialize typeDoc to "ordonnace"
-        private string typeDoc = "ordonnance";
+	public class HomeViewModel : ViewModelBase
+	{
+		//type of photo, most of the time ordonannces but we can also have document
+		//so we initialize typeDoc to "ordonnace"
+		private string typeDoc = "ordonnance";
 		private string _documentPath = string.Empty;
-        private string AdvisorContact = string.Empty;
+		private string AdvisorContact = string.Empty;
 
 
-        public HomeViewModel()
-        {
-            SubscribeToMessages();
-            CheckForAdvisor();
-        }
-        public ICommand AssistCommand => new Command(async () =>
-        {
-            var action = await CoreMethods.DisplayActionSheet(null, "Annuler", null, "Appel vocal", "E-mail", "SMS");
-            switch (action)
-            {
-                case "Appel vocal":
-                    Device.OpenUri(new System.Uri($"tel:{Settings.AdvisorContact}"));
-                    break;
+		public HomeViewModel()
+		{
+			SubscribeToMessages();
+			CheckForAdvisor();
+		}
+		public ICommand AssistCommand => new Command(async () =>
+		{
+			var action = await CoreMethods.DisplayActionSheet(null, "Annuler", null, "Appel vocal", "E-mail", "SMS");
+			switch (action)
+			{
+				case "Appel vocal":
+					Device.OpenUri(new System.Uri($"tel:{Settings.AdvisorContact}"));
+					break;
 
-                case "E-mail":
-                    Device.OpenUri(new System.Uri($"mailto:{Settings.AdvisorEmail}"));
-                    break;
+				case "E-mail":
+					Device.OpenUri(new System.Uri($"mailto:{Settings.AdvisorEmail}"));
+					break;
 
-                case "SMS":
-                    Device.OpenUri(new System.Uri($"sms:{Settings.AdvisorContact}"));
-                    break;
-            }
+				case "SMS":
+					Device.OpenUri(new System.Uri($"sms:{Settings.AdvisorContact}"));
+					break;
+			}
 
-        });
+		});
 
-        private async Task CheckForAdvisor()
-        {
-            var request = new GetListRequest(20, 0);
-            var userInfo = await App.UserManager.GetAsync($"{JsonConvert.DeserializeObject<User>(Settings.CurrentUser).Id}/advisor");
-            Settings.AdvisorContact = AdvisorContact = userInfo != null ? userInfo.Phone : string.Empty;
-            Settings.AdvisorEmail = userInfo != null ? userInfo.Email : string.Empty;
+		private async Task CheckForAdvisor()
+		{
+			var request = new GetListRequest(20, 0);
+			var userInfo = await App.UserManager.GetAsync($"{JsonConvert.DeserializeObject<User>(Settings.CurrentUser).Id}/advisor");
+			Settings.AdvisorContact = AdvisorContact = userInfo != null ? userInfo.Phone : string.Empty;
+			Settings.AdvisorEmail = userInfo != null ? userInfo.Email : string.Empty;
 
-        }
+		}
 
-        //This concern only ordonances  in normal process
-        public ICommand AddOrdonnanceCommand => new Command(async () =>
-        {
-            var action = await CoreMethods.DisplayActionSheet(null, "Annuler", null, "Appareil photo", "Bibliothèque photo");
-            if (action == "Appareil photo")
-            {
-                await CrossMedia.Current.Initialize();
+		//This concern only ordonances  in normal process
+		public ICommand AddOrdonnanceCommand => new Command(async () =>
+		{
+			var action = await CoreMethods.DisplayActionSheet(null, "Annuler", null, "Appareil photo", "Bibliothèque photo");
+			if (action == "Appareil photo")
+			{
+				await CrossMedia.Current.Initialize();
 
-                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-                {
-                    await CoreMethods.DisplayAlert("L'appareil photo n'est pas disponible", null, "OK");
-                    return;
-                }
+				if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+				{
+					await CoreMethods.DisplayAlert("L'appareil photo n'est pas disponible", null, "OK");
+					return;
+				}
 
-                var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions());
-                //if photo ok
-                if (file != null)
-                {
-                    var profilePicture = ImageSource.FromStream(() => file.GetStream());
-                    var typeNavigation = "normal";
+				var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions());
+				//if photo ok
+				if (file != null)
+				{
+					var profilePicture = ImageSource.FromStream(() => file.GetStream());
+					var typeNavigation = "normal";
 
-                    await CoreMethods.PushPageModel<PatientListViewModel>(new string[] { "HomeSelectPatient", typeNavigation, typeDoc }, true);
+					await CoreMethods.PushPageModel<PatientListViewModel>(new string[] { "HomeSelectPatient", typeNavigation, typeDoc }, true);
 
-                    //var page = FreshPageModelResolver.ResolvePageModel<PatientListViewModel>();
-                    //var basicNavContainer = new FreshNavigationContainer(page, "HomeSelectPatient");
-                    //await CoreMethods.PushNewNavigationServiceModal(basicNavContainer, new FreshBasePageModel[] { page.GetModel() });
-                }
-            }
-            else if (action == "Bibliothèque photo")
-            {
-                await CrossMedia.Current.Initialize();
+					//var page = FreshPageModelResolver.ResolvePageModel<PatientListViewModel>();
+					//var basicNavContainer = new FreshNavigationContainer(page, "HomeSelectPatient");
+					//await CoreMethods.PushNewNavigationServiceModal(basicNavContainer, new FreshBasePageModel[] { page.GetModel() });
+				}
+			}
+			else if (action == "Bibliothèque photo")
+			{
+				await CrossMedia.Current.Initialize();
 
 				var pickerOptions = new PickMediaOptions();
 
-                var file = await CrossMedia.Current.PickPhotoAsync(pickerOptions);
-                if (file != null)
-                {
-                    var profilePicture = ImageSource.FromStream(() => file.GetStream());
-                    var typeNavigation = "normal";
+				var file = await CrossMedia.Current.PickPhotoAsync(pickerOptions);
+				if (file != null)
+				{
+					var profilePicture = ImageSource.FromStream(() => file.GetStream());
+					var typeNavigation = "normal";
 					_documentPath = file.Path;
-                    await CoreMethods.PushPageModel<PatientListViewModel>(new string[] { "HomeSelectPatient", typeNavigation, typeDoc },true);
+					await CoreMethods.PushPageModel<PatientListViewModel>(new string[] { "HomeSelectPatient", typeNavigation, typeDoc }, true);
 
-                    //var page = FreshPageModelResolver.ResolvePageModel<PatientListViewModel>();
-                    //var basicNavContainer = new FreshNavigationContainer(page, "HomeSelectPatient");
-                    //await CoreMethods.PushNewNavigationServiceModal(basicNavContainer, new FreshBasePageModel[] { page.GetModel() });
+					//var page = FreshPageModelResolver.ResolvePageModel<PatientListViewModel>();
+					//var basicNavContainer = new FreshNavigationContainer(page, "HomeSelectPatient");
+					//await CoreMethods.PushNewNavigationServiceModal(basicNavContainer, new FreshBasePageModel[] { page.GetModel() });
 
-                }
-            }
-        });
+				}
+			}
+		});
 
-        //This concern  ordonances or documents in fast process
-        public ICommand FastCommand => new Command(async () =>
-        {
-            var action = await CoreMethods.DisplayActionSheet(null, "Annuler", null, "Ordonnance", "Document");
+		//This concern  ordonances or documents in fast process
+		public ICommand FastCommand => new Command(async () =>
+		{
+			var action = await CoreMethods.DisplayActionSheet(null, "Annuler", null, "Ordonnance", "Document");
 
-            if ((action == "Ordonnance") || (action == "Document"))
-            {
-                await CrossMedia.Current.Initialize();
+			if ((action == "Ordonnance") || (action == "Document"))
+			{
+				await CrossMedia.Current.Initialize();
 
-                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-                {
-                    await CoreMethods.DisplayAlert("L'appareil photo n'est pas disponible", null, "OK");
-                    return;
-                }
+				if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+				{
+					await CoreMethods.DisplayAlert("L'appareil photo n'est pas disponible", null, "OK");
+					return;
+				}
 
-                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-				{ Directory="Docs",Name=DateTime.Now.Ticks.ToString() });
-                if (file != null)
-                {
-                    //if document we change typeDoc to document
-                    if (action == "Document") { typeDoc = "document"; }
+				var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+				{ Directory = "Docs", Name = DateTime.Now.Ticks.ToString() });
+				if (file != null)
+				{
+					//if document we change typeDoc to document
+					if (action == "Document") { typeDoc = "document"; }
 
-                    var profilePicture = ImageSource.FromStream(() => file.GetStream());
-                    var typeNavigation = "fast";
+					var profilePicture = ImageSource.FromStream(() => file.GetStream());
+					var typeNavigation = "fast";
 					_documentPath = file.Path;
-                    await CoreMethods.PushPageModel<PatientListViewModel>(new string[] { "HomeSelectPatient", typeNavigation, typeDoc }, true);
+					await CoreMethods.PushPageModel<PatientListViewModel>(new string[] { "HomeSelectPatient", typeNavigation, typeDoc }, true);
 
-                    //var page = FreshPageModelResolver.ResolvePageModel<PatientListViewModel>();
-                    //var basicNavContainer = new FreshNavigationContainer(page, "HomeSelectPatient");
-                    //await CoreMethods.PushNewNavigationServiceModal(basicNavContainer, new FreshBasePageModel[] { page.GetModel() });
-                }
-            }
-        });
+					//var page = FreshPageModelResolver.ResolvePageModel<PatientListViewModel>();
+					//var basicNavContainer = new FreshNavigationContainer(page, "HomeSelectPatient");
+					//await CoreMethods.PushNewNavigationServiceModal(basicNavContainer, new FreshBasePageModel[] { page.GetModel() });
+				}
+			}
+		});
 
-        private void SubscribeToMessages()
-        {
-            MessagingCenter.Subscribe<PatientListViewModel, Patient>(this, Events.HomePageSetPatientForOrdonnance,
-                async (sender, patient) =>
-                {
-                    if (patient != null)
-                    {
-                        await AddPrescription(patient);
-                    }
-                });
-        }
+		private void SubscribeToMessages()
+		{
+			MessagingCenter.Subscribe<PatientListViewModel, Patient>(this, Events.HomePageSetPatientForOrdonnance,
+				async (sender, patient) =>
+				{
+					if (patient != null)
+					{
+						await AddPrescription(patient);
+					}
+				});
+		}
 
-        private async Task AddPrescription(Patient patient)
-        {
-            if(typeDoc== "ordonnance")
-            {
-                var ordannance = new Ordonnance()
-                {
-                    Reference = DateTime.Now.Ticks,
-                    First_Care_At = App.ConvertToUnixTimestamp(DateTime.Now),
-                    Patient = patient,
-                    Status = Enums.StatusEnum.valid,
-                    Id = DateTime.Now.Ticks.ToString(),
-                    CreatedAt = DateTimeOffset.Now,
-                    Attachments = new List<string>() { _documentPath },
-                    Frequencies = new List<Frequency>()
-                };
-                await new StorageService<Ordonnance>().AddAsync(ordannance);
-            }
-            else
-            {
-                var document = new Document()
-                {
-                    Reference = DateTime.Now.Ticks,
-                    AddDate = DateTime.Now,
-                    Patient = patient,
-                    AttachmentPath = _documentPath,
-                    Id = DateTime.Now.Ticks.ToString(),
-                    CreatedAt = DateTimeOffset.Now,
-                };
-                await new StorageService<Document>().AddAsync(document);
-            }
-            
-        }
-    }
+		private async Task AddPrescription(Patient patient)
+		{
+			if (typeDoc == "ordonnance")
+			{
+				var ordannance = new Ordonnance()
+				{
+					Id = Guid.NewGuid().ToString(),
+					CreatedAt = DateTime.Today,
+					First_Care_At = App.ConvertToUnixTimestamp(DateTime.Now),
+					Attachments = new List<string>() { _documentPath },
+					Frequencies = new List<Frequency>(),
+					Patient = patient,
+					PatientId = patient.Id,
+					PatientName = patient.Fullname
+				};
+				await new StorageService<Ordonnance>().AddAsync(ordannance);
+			}
+			else
+			{
+				var document = new Document()
+				{
+					Id = Guid.NewGuid().ToString(),
+					CreatedAt = DateTime.Today,
+					Reference = DateTime.Now.Ticks,
+					AddDate = DateTime.Now,
+					Patient = patient,
+					PatientId = patient.Id,
+					AttachmentPath = _documentPath,
+				};
+				await new StorageService<Document>().AddAsync(document);
+			}
+
+		}
+	}
 }
