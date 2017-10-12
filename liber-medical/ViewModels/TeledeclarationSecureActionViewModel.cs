49b1461de2f0cd1759fc3ba46f57b79248b8f0df
Dhillu <dhillu143@gmail.com>
2017-10-12 11:55:17 +0530

    implemented validate & refuse buttons functionality

--- liber-medical/ViewModels/TeledeclarationSecureActionViewModel.cs
+++ liber-medical/ViewModels/TeledeclarationSecureActionViewModel.cs
@@ -2,34 +2,77 @@
 using FreshMvvm;
 using libermedical.Models;
 using libermedical.Services;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace libermedical.ViewModels
 {
-	public class TeledeclarationSecureActionViewModel:FreshBasePageModel
-	{
-		private readonly IStorageService<Teledeclaration> _teledeclarationService;
+    public class TeledeclarationSecureActionViewModel : FreshBasePageModel
+    {
+        private IStorageService<Teledeclaration> _teledeclarationService;
 
-		private Teledeclaration _teledeclaration;
+        private Teledeclaration _teledeclaration;
 
-		public Teledeclaration Teledeclaration
-		{
-			get { return _teledeclaration;}
-			set
-			{
-				_teledeclaration = value;
-				if (_teledeclaration != null)
-					CanValidate = _teledeclaration.Status == Enums.StatusEnum.waiting ? true : false;
-				RaisePropertyChanged();
-			}
-		}
-		public bool CanValidate { get; set; }
+        public Teledeclaration Teledeclaration
+        {
+            get { return _teledeclaration; }
+            set
+            {
+                _teledeclaration = value;
+                if (_teledeclaration != null)
+                    CanValidate = _teledeclaration.Status == Enums.StatusEnum.waiting ? true : false;
+                RaisePropertyChanged();
+            }
+        }
+        public TeledeclarationSecureActionViewModel()
+        {
+            SubscribeMessages();
+        }
+        public bool CanValidate { get; set; }
 
-		public override void Init(object initData)
-		{
-			base.Init(initData);
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+        }
 
+        public ICommand ValidateCommand
+        {
+            get
+            {
+                return new Command(async (args) =>
+                {
 
-		}
+                    if (args as string == "Valid")
+                        Teledeclaration.Status = Enums.StatusEnum.valid;
+                    else
+                        Teledeclaration.Status = Enums.StatusEnum.refused;
 
-	}
+                    _teledeclarationService = new StorageService<Teledeclaration>();
+                    await _teledeclarationService.DeleteItemAsync(typeof(Teledeclaration).Name + "_" + Teledeclaration.Id);
+                    await _teledeclarationService.AddAsync(Teledeclaration);
+
+                    //TODO: Display success toast
+
+                    await CoreMethods.PopPageModel();
+                });
+            }
+        }
+
+        public ICommand CloseCommand
+        {
+            get { return new Command(async() => { await CoreMethods.PopPageModel(); }); }
+        }
+
+        private void SubscribeMessages()
+        {
+            MessagingCenter.Subscribe<TeledeclarationsListViewModel, Teledeclaration>(this, Events.UpdateTeledeclarationDetailsPage,
+                (sender, teledeclaration) =>
+                {
+                    if (teledeclaration != null)
+                    {
+                        Teledeclaration = teledeclaration;
+                    }
+                });            
+        }
+    }
 }
