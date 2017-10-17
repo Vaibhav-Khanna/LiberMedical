ddaadecdde0ff7ec909a0851dc4693ccad5428a4
dhillu <dhillu@DHILLUs-MacBook-Pro.local>
2017-10-17 22:05:45 +0530

    filters implemented in Ordonnances & Teledeclarations

--- liber-medical/Models/Filter.cs
+++ liber-medical/Models/Filter.cs
@@ -10,5 +10,10 @@ namespace libermedical.Models
         public DateTime EndDate { get; set; }
         public List<StatusEnum> Statuses { get; set; }
         public bool IsActivated { get; set; }
+		public bool EnableDateSearch
+		{
+			get;
+			set;
+		}
     }
 }
--- liber-medical/Pages/FilterPage.xaml.cs
+++ liber-medical/Pages/FilterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using libermedical.CustomControls;
 using libermedical.Enums;
 using libermedical.Models;
@@ -6,76 +7,96 @@ using Xamarin.Forms;
 
 namespace libermedical.Pages
 {
-    public partial class FilterPage : BasePage
-    {
-        private Filter _filter;
+	public partial class FilterPage : BasePage
+	{
+		private Filter _filter;
 		private string _parentScreen;
 
-        public FilterPage(string parentScreen,Filter filter = null) : base(-1, 64, false)
-        {
-            InitializeComponent();
+		public FilterPage(string parentScreen, Filter filter = null) : base(-1, 64, false)
+		{
+			InitializeComponent();
 			_parentScreen = parentScreen;
-            if (filter != null)
-            {
-                StartDatePicker.Date = filter.StartDate;
-                EndDatePicker.Date = filter.EndDate;
-                if (filter.Statuses.Contains(StatusEnum.waiting))
-                {
-                    attente.On = true;
-                }
-                if (filter.Statuses.Contains(StatusEnum.valid))
-                {
-                    traite.On = true;
-                }
-                if (filter.Statuses.Contains(StatusEnum.refused))
-                {
-                    refuse.On = true;
-                }
-            }
-        }
+			if (filter != null)
+			{
+				_filter = filter;
+				StartDatePicker.Date = filter.StartDate;
+				EndDatePicker.Date = filter.EndDate;
+				if (filter.Statuses.Contains(StatusEnum.waiting))
+				{
+					attente.On = true;
+				}
+				if (filter.Statuses.Contains(StatusEnum.valid))
+				{
+					traite.On = true;
+				}
+				if (filter.Statuses.Contains(StatusEnum.refused))
+				{
+					refuse.On = true;
+				}
 
-        async void Back_Tapped(object sender, System.EventArgs e)
-        {
-            await Navigation.PopModalAsync();
-        }
+			}
+			else
+			{
+				_filter = new Filter
+				{
+					StartDate = StartDatePicker.Date,
+					EndDate = EndDatePicker.Date,
+					Statuses = new List<StatusEnum>()
+				};
+			}
+		}
 
-        async void Reset_Tapped(object sender, System.EventArgs e)
-        {
-            _filter = null;
+		async void Back_Tapped(object sender, System.EventArgs e)
+		{
+			await Navigation.PopModalAsync();
+		}
 
-            MessagingCenter.Send(this, Events.UpdatePrescriptionFilters, _filter);
+		async void Reset_Tapped(object sender, System.EventArgs e)
+		{
+			_filter = null;
 
-            await Navigation.PopModalAsync();
-        }
+			MessagingCenter.Send(this, Events.UpdatePrescriptionFilters, _filter);
 
-        async void Search_Tapped(object sender, System.EventArgs e)
-        {
-            _filter = new Filter
-            {
-                StartDate = StartDatePicker.Date,
-                EndDate = EndDatePicker.Date,
-                Statuses = new List<StatusEnum>()
-            };
-            if (attente.On)
-            {
-                _filter.Statuses.Add(StatusEnum.waiting);
-            }
-            if (traite.On)
-            {
-                _filter.Statuses.Add(StatusEnum.valid);
-            }
-            if (refuse.On)
-            {
-                _filter.Statuses.Add(StatusEnum.refused);
-            }
-            if (_filter.Statuses.Count > 1)
-                _filter.IsActivated = true;
+			await Navigation.PopModalAsync();
+		}
 
-            MessagingCenter.Send(this, Events.UpdatePrescriptionFilters, _filter);
-			if(_parentScreen=="Teledeclarations")
-			MessagingCenter.Send(this, Events.UpdateTeledeclarationsFilters, _filter);
+		public void Handle_EndDateSelected(object sender, Xamarin.Forms.DateChangedEventArgs e)
+		{
+			_filter.IsActivated = true;
+			_filter.EnableDateSearch = true;
+			_filter.EndDate = ((DatePicker)sender).Date;
+		}
 
-            await Navigation.PopModalAsync();
-        }
-    }
+		public void Handle_StartDateSelected(object sender, Xamarin.Forms.DateChangedEventArgs e)
+		{
+			_filter.IsActivated = true;
+			_filter.EnableDateSearch = true;
+			_filter.StartDate = ((DatePicker)sender).Date;
+		}
+
+		async void Search_Tapped(object sender, System.EventArgs e)
+		{
+
+			if (attente.On)
+			{
+				_filter.Statuses.Add(StatusEnum.waiting);
+			}
+			if (traite.On)
+			{
+				_filter.Statuses.Add(StatusEnum.valid);
+			}
+			if (refuse.On)
+			{
+				_filter.Statuses.Add(StatusEnum.refused);
+			}
+			if (_filter.Statuses.Count >= 1)
+				_filter.IsActivated = true;
+
+			MessagingCenter.Send(this, Events.UpdatePrescriptionFilters, _filter);
+			if (_parentScreen == "Teledeclarations")
+				MessagingCenter.Send(this, Events.UpdateTeledeclarationsFilters, _filter);
+
+			await Navigation.PopModalAsync();
+		}
+	}
 }
