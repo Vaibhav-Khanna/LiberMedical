7450dff80950b7321504e8edd78c4eb101131f61
Dhillu <dhillu143@gmail.com>
2017-10-20 13:34:07 +0530

    Implemented Non-Scrollable list and updated UI and UX glitches

---
+++ iOS/Renderers/NonScrollableListRenderer.cs
@@ -0,0 +1,18 @@
+﻿using libermedical.CustomControls;
+using libermedical.iOS.Renderers;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NonScrollableList), typeof(NonScrollableListRenderer))]
+namespace libermedical.iOS.Renderers
+{
+    public class NonScrollableListRenderer : ListViewRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            Control.ScrollEnabled = false;
+        }
+    }
+}
