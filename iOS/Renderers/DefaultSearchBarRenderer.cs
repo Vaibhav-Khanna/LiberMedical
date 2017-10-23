23091c7c4f8a64627824d728bf05a732aebd4cd4
Vaibhav Khanna <y2kvaibhav@gmail.com>
2017-10-23 21:06:21 +0530

    Init

---
+++ iOS/Renderers/DefaultSearchBarRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using libermedical.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(SearchBar),typeof(DefaultSearchBarRenderer))]
+namespace libermedical.iOS.Renderers
+{
+    public class DefaultSearchBarRenderer : SearchBarRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+
+
+            if(Control!=null)
+            {
+                Control.ShowsCancelButton = false;
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            Control.ShowsCancelButton = false;
+        }
+
+    }
+}
