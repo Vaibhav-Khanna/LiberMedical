520381c067aae34e1e6abd8b4daa3f55ed9521af
Vaibhav Khanna <y2kvaibhav@gmail.com>
2018-06-29 17:32:51 +0200

    Flushing data at Logout

--- liber-medical/Pages/PlusPage.xaml.cs
+++ liber-medical/Pages/PlusPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using libermedical.Helpers;
 using Xamarin.Forms;
+using Akavache;
+using System.Reactive.Linq;
+using Acr.UserDialogs;
 
 namespace libermedical.Pages
 {
@@ -12,9 +15,7 @@ namespace libermedical.Pages
             ToolbarItems.Add(new ToolbarItem("Déconnexion","", HandleAction));
 
         }
-
-      
-
+              
         protected override void OnAppearing()
         {
             base.OnAppearing();
@@ -25,15 +26,25 @@ namespace libermedical.Pages
 
         async void HandleAction()
         {
-            var response = await DisplayAlert("Déconnexion", "Êtes-vous sûr de vouloir vous déconnecter ?", "Oui", "Annuler");
+            var response = await DisplayAlert("Déconnexion", "Êtes-vous sûr de vouloir vous déconnecter ? Tous les fichiers non synchronisés seraient perdus.", "Oui", "Annuler");
 
             if (response)
             {
                 Settings.Token = string.Empty;
                 Settings.TokenExpiration = 0;
                 Settings.IsLoggedIn = false;
+
+                if (await Plugin.Connectivity.CrossConnectivity.Current.IsRemoteReachable("https://www.google.com"))
+                {
+                    UserDialogs.Instance.ShowLoading("Synchronisation des modifications...");
+                    await App.SyncData();
+                    UserDialogs.Instance.HideLoading();
+                }
+
+                await BlobCache.UserAccount.InvalidateAll();
                 Application.Current.MainPage = new NavigationPage(new LoginPage());
             }
         }
+
     }
 }
